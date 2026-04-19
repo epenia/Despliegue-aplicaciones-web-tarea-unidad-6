97ae53b0d7c221ead29d64730f205a19c9f414ee
Endika <epenia@endikapenia.es>
2026-04-19 11:32:29 +0000

    Documentación del código

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Tarea",
+            "name": "Tarea",
+            "summary": "Representa\u0020una\u0020tarea\u0020en\u0020el\u0020sistema.",
+            "url": "classes/Tarea.html"
+        },                {
+            "fqsen": "\\Tarea\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "Constructor\u0020de\u0020la\u0020clase\u0020Tarea.",
+            "url": "classes/Tarea.html#method___construct"
+        },                {
+            "fqsen": "\\Tarea\u003A\u003AgetNombre\u0028\u0029",
+            "name": "getNombre",
+            "summary": "Obtiene\u0020el\u0020nombre\u0020de\u0020la\u0020tarea.",
+            "url": "classes/Tarea.html#method_getNombre"
+        },                {
+            "fqsen": "\\Tarea\u003A\u003A\u0024nombre",
+            "name": "nombre",
+            "summary": "",
+            "url": "classes/Tarea.html#property_nombre"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
